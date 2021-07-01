fd4368b6d6af8520be0ec37d1068335afb877ce5
musa <Musaz@shandonbiz.com>
2021-07-01 20:06:40 +0200

    Unit tests
    Added unit tests

---
+++ source/ProductInventory.Tests/DB/Domain/TestEntityBase.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using PeanutButter.TestUtils.Generic;
+using ProductsInventory.DB.Domain;
+using System;
+
+namespace ProductInventory.Tests.DB.Domain
+{
+    [TestFixture]
+    public class TestEntityBase
+    {
+        [Test]
+        public void Construct()
+        {
+            //---------------Set up test pack-------------------
+            //---------------Assert Precondition----------------
+            //---------------Execute Test ----------------------
+            Assert.DoesNotThrow(() => new EntityBase());
+            //---------------Test Result -----------------------
+        }
+
+        [TestCase("CreatedBy", typeof(string))]
+        [TestCase("LastUpdatedBy", typeof(string))]
+        [TestCase("DateCreated", typeof(DateTime?))]
+        [TestCase("DateLastModified", typeof(DateTime?))]
+        public void EntityBase_ShouldHaveProperty(string propertyName, Type propertyType)
+        {
+            //---------------Set up test pack-------------------
+            var sut = typeof(EntityBase);
+            //---------------Assert Precondition----------------
+            //---------------Execute Test ----------------------
+            sut.ShouldHaveProperty(propertyName, propertyType);
+            //---------------Test Result -----------------------
+        }
+    }
+}
---
+++ source/ProductsInventory.DB/Domain/EntityBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProductsInventory.DB.Domain
+{
+    public class EntityBase
+    {
+        public string CreatedBy { get; set; }
+        public string LastUpdatedBy { get; set; }
+        public DateTime? DateCreated { get; set; }
+        public DateTime? DateLastModified { get; set; }
+    }
+}
