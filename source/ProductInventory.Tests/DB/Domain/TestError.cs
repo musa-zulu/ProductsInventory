5cda8eda863c1ec20d1db9b51e32934b2a126f7f
musa <Musaz@shandonbiz.com>
2021-07-01 20:28:08 +0200

    Added Error class
    Added a class that will be used to store the errors on the db

---
+++ source/ProductInventory.Tests/DB/Domain/TestError.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using PeanutButter.TestUtils.Generic;
+using ProductsInventory.DB.Domain;
+using System;
+
+namespace ProductInventory.Tests.DB.Domain
+{
+    [TestFixture]
+        public class TestError
+        {
+            [Test]
+            public void Construct()
+            {
+                //---------------Set up test pack-------------------
+                //---------------Assert Precondition----------------
+                //---------------Execute Test ----------------------
+                Assert.DoesNotThrow(() => new Error());
+                //---------------Test Result -----------------------
+            }
+
+            [TestCase("ErrorId", typeof(Guid))]
+            [TestCase("Message", typeof(string))]
+            [TestCase("StackTrace", typeof(string))]
+            [TestCase("LastUpdatedBy", typeof(string))]
+            [TestCase("DateCreated", typeof(DateTime?))]
+            [TestCase("DateLastModified", typeof(DateTime?))]
+            public void Error_ShouldHaveProperty(string propertyName, Type propertyType)
+            {
+                //---------------Set up test pack-------------------
+                var sut = typeof(Error);
+                //---------------Assert Precondition----------------
+                //---------------Execute Test ----------------------
+                sut.ShouldHaveProperty(propertyName, propertyType);
+                //---------------Test Result -----------------------
+            }
+        }
+    }
---
+++ source/ProductsInventory.DB/Domain/Error.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ProductsInventory.DB.Domain
+{
+    public class Error : EntityBase
+    {
+        public Guid ErrorId { get; set; }
+        public string Message { get; set; }
+        public string StackTrace { get; set; }
+    }
+}
