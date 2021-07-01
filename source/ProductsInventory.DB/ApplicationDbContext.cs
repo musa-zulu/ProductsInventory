944dcc06d2688bc6d8f2c280b643c91aa81ea693
musa <Musaz@shandonbiz.com>
2021-07-01 20:07:07 +0200

    DBContext
    Added application db context

---
+++ source/ProductsInventory.DB/ApplicationDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace ProductsInventory.DB
+{
+    public interface IApplicationDbContext
+    {
+        Task<int> SaveChangesAsync();
+    }
+    public sealed class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        private readonly IConfiguration _config;
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IConfiguration config)
+            : base(options)
+        {
+            _config = config;
+        }
+
+        public Task<int> SaveChangesAsync() => base.SaveChangesAsync();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlServer(_config.GetConnectionString("DefaultConnection"));
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+     
+        }
+    }
+}
