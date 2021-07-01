f45343890b6e85b7ce4748ac02c07955259fe4c1
musa <Musaz@shandonbiz.com>
2021-07-01 19:41:39 +0200

    Client Solution
    Added client solution

---
+++ documents/scripts/bootstrap.sql
@@ -0,0 +1,42 @@
+-- use master;
+-- go
+-- ALTER DATABASE  [ProductsInventory]
+-- SET SINGLE_USER
+-- WITH ROLLBACK IMMEDIATE
+-- drop database [ProductsInventory]
+-- go
+------ normal creation after here
+use master;
+go
+if not exists (select name from master..syslogins where name = 'ProductsInventoryWeb')
+    begin
+        create login ProductsInventoryWeb with password = 'P4$$w0rd';
+    end;
+go
+
+
+if not exists (select name from master..sysdatabases where name = 'ProductsInventory')
+begin
+create database OnlineStore
+end;
+GO
+
+use ProductsInventory
+if not exists (select * from sysusers where name = 'ProductsInventoryWeb')
+begin
+create user ProductsInventoryWeb
+	for login ProductsInventoryWeb
+	with default_schema = dbo
+end;
+GO
+grant connect to ProductsInventoryWeb
+go
+exec sp_addrolemember N'db_datareader', N'ProductsInventoryWeb';
+go
+exec sp_addrolemember N'db_datawriter', N'ProductsInventoryWeb';
+go
+exec sp_addrolemember N'db_owner', N'ProductsInventoryWeb';
+GO
+use master;
+GO
+
