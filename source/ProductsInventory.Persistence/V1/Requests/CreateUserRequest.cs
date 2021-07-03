761d751322a246f4b0d53d95fc30fa28bdb55b0f
musa <Musaz@shandonbiz.com>
2021-07-03 14:33:17 +0200

    Create user request
    added CreateUserRequest and tests

---
+++ source/ProductsInventory.Persistence/V1/Requests/CreateUserRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ProductsInventory.Persistence.V1.Requests
+{
+    public class CreateUserRequest : RequestBase
+    {
+        public Guid UserId { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string HashedPassword { get; set; }
+        public string Salt { get; set; }
+        public bool IsLocked { get; set; }
+    }
+}
