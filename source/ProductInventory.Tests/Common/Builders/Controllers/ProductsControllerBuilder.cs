3114e56438da80bc5c66ee46051dbaa588f2193c
musa <Musaz@shandonbiz.com>
2021-07-04 17:12:37 +0200

    builders
    added builders

---
+++ source/ProductInventory.Tests/Common/Builders/Controllers/ProductsControllerBuilder.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using NSubstitute;
+using ProductsInventory.Persistence.Interfaces.Services;
+using ProductsInventory.Server.Controllers.V1;
+
+namespace ProductInventory.Tests.Common.Builders.Controllers
+{
+    public class ProductsControllerBuilder
+    {
+        public ProductsControllerBuilder()
+        {
+            Mapper = Substitute.For<IMapper>();
+            UriService = Substitute.For<IUriService>();
+            ProductService = Substitute.For<IProductService>();
+        }
+
+        public IMapper Mapper { get; private set; }
+        public IUriService UriService { get; private set; }
+        public IProductService ProductService { get; private set; }
+
+        public ProductsControllerBuilder WithMapper(IMapper mapper)
+        {
+            Mapper = mapper;
+            return this;
+        }
+
+        public ProductsControllerBuilder WithUriService(IUriService uriService)
+        {
+            UriService = uriService;
+            return this;
+        }
+
+        public ProductsControllerBuilder WithProductService(IProductService productService)
+        {
+            ProductService = productService;
+            return this;
+        }
+
+        public ProductsController Build()
+        {
+            return new ProductsController(ProductService, Mapper, UriService);
+        }
+    }
+}
